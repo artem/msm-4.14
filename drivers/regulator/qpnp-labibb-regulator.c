5294a056ed322ed7bb4b35bac2bdee67997b435f
Subbaraman Narayanamurthy <subbaram@codeaurora.org>
2018-05-01 18:09:09 -0700

    regulator: qpnp-labibb: Add standby mode support in AOD
    Currently, REGULATOR_MODE_IDLE supports switching to AOD mode
    and REGULATOR_MODE_NORMAL supports switching back to normal
    operational mode. When switching from AOD to OFF mode, SCP and
    PD settings should be set similar to normal mode however it is
    not needed to wait for VREG_OK to be set. Add support for this
    through REGULATOR_MODE_STANDBY.
    
    Change-Id: Ice05145bd53093ce46775a4f2a06d2c2b01d93c2
    Signed-off-by: Subbaraman Narayanamurthy <subbaram@codeaurora.org>

--- drivers/regulator/qpnp-labibb-regulator.c
+++ drivers/regulator/qpnp-labibb-regulator.c
@@ -2678,7 +2678,7 @@ static int qpnp_lab_regulator_set_mode(struct regulator_dev *rdev,
 		return 0;
 
 	/* AVDD_MIN voltage = 5.65 + code * 0.15 V */
-	if (mode == REGULATOR_MODE_NORMAL)
+	if (mode == REGULATOR_MODE_NORMAL || mode == REGULATOR_MODE_STANDBY)
 		val = 0xD;
 	else if (mode == REGULATOR_MODE_IDLE)
 		val = 0x1;
@@ -2707,6 +2707,19 @@ static int qpnp_lab_regulator_set_mode(struct regulator_dev *rdev,
 			goto out;
 
 		rc = qpnp_lab_scp_control(labibb, false);
+		if (rc < 0)
+			goto out;
+	} else if (mode == REGULATOR_MODE_STANDBY) {
+		cancel_work_sync(&labibb->aod_lab_vreg_ok_work);
+		rc = qpnp_ibb_pd_control(labibb, true);
+		if (rc < 0)
+			goto out;
+
+		rc = qpnp_lab_pd_control(labibb, true);
+		if (rc < 0)
+			goto out;
+
+		rc = qpnp_lab_scp_control(labibb, true);
 		if (rc < 0)
 			goto out;
 	}
@@ -3343,7 +3356,8 @@ static int register_qpnp_lab_regulator(struct qpnp_labibb *labibb,
 			init_data->constraints.valid_ops_mask |=
 				REGULATOR_CHANGE_MODE;
 			init_data->constraints.valid_modes_mask |=
-				REGULATOR_MODE_NORMAL | REGULATOR_MODE_IDLE;
+				REGULATOR_MODE_NORMAL | REGULATOR_MODE_IDLE |
+				REGULATOR_MODE_STANDBY;
 			labibb->lab_vreg.mode = REGULATOR_MODE_NORMAL;
 		}
 
