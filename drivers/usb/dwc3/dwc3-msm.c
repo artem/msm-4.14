c826ad4a7335b092f3a6e379ee36985ad0fbba55
Badhri Jagan Sridharan <badhri@google.com>
2018-05-03 16:45:07 -0700

    msm-dwc3: Add node to enable/disable ext_vbus switching
    This CL adds node to enable/disable switching to external
    vbus. VBUS is sourced from the external vbus reg when
    power consumed by the device is less than
    auto_vbus_src_sel_threshold.
    
    Disable:
    echo 0 >
    /sys/devices/plaform/soc/a600000.ssusb/auto_vbus_src_sel_threshold
    Enable:
    echo 250 >
    /sys/devices/plaform/soc/a600000.ssusb/auto_vbus_src_sel_threshold
    
    Bug:79136281
    Change-Id: I8c0827b1c43045048c9a1157a87615a28a235339
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -327,7 +327,7 @@ struct dwc3_msm {
 	struct notifier_block	dpdm_nb;
 	struct regulator	*dpdm_reg;
 
-	u32 auto_vbus_src_sel;
+	u32 auto_vbus_src_sel_threshold;
 };
 
 #define USB_HSPHY_3P3_VOL_MIN		3050000 /* uV */
@@ -3452,6 +3452,33 @@ static int dwc_dpdm_cb(struct notifier_block *nb, unsigned long evt, void *p)
 
 	return NOTIFY_OK;
 }
+
+static ssize_t auto_vbus_src_sel_threshold_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct dwc3_msm *mdwc = dev_get_drvdata(dev);
+
+	return snprintf(buf, PAGE_SIZE, "%u\n",
+		mdwc->auto_vbus_src_sel_threshold);
+}
+
+static ssize_t auto_vbus_src_sel_threshold_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	int ret = 0;
+	struct dwc3_msm *mdwc = dev_get_drvdata(dev);
+
+
+	ret = kstrtou32(buf, 10, &mdwc->auto_vbus_src_sel_threshold);
+
+	if (ret)
+		return ret;
+
+	return count;
+}
+static DEVICE_ATTR_RW(auto_vbus_src_sel_threshold);
+
+
 static int dwc3_msm_probe(struct platform_device *pdev)
 {
 	struct device_node *node = pdev->dev.of_node, *dwc3_node;
@@ -3510,10 +3537,11 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 				"qcom,charging-disabled");
 
 	ret = of_property_read_u32(node, "google,switch-vbus",
-				   &mdwc->auto_vbus_src_sel);
+				   &mdwc->auto_vbus_src_sel_threshold);
 	if (ret) {
-		dev_dbg(&pdev->dev, "setting auto_vbus_src_sel to zero.\n");
-		mdwc->auto_vbus_src_sel = 0;
+		dev_dbg(&pdev->dev,
+			"setting auto_vbus_src_sel_threshold to zero.\n");
+		mdwc->auto_vbus_src_sel_threshold = 0;
 	}
 
 	ret = of_property_read_u32(node, "qcom,lpm-to-suspend-delay-ms",
@@ -3809,6 +3837,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 	device_create_file(&pdev->dev, &dev_attr_speed);
 	device_create_file(&pdev->dev, &dev_attr_usb_compliance_mode);
 	device_create_file(&pdev->dev, &dev_attr_bus_vote);
+	device_create_file(&pdev->dev, &dev_attr_auto_vbus_src_sel_threshold);
 
 	return 0;
 
@@ -3836,6 +3865,7 @@ static int dwc3_msm_remove(struct platform_device *pdev)
 	int ret_pm;
 
 	device_remove_file(&pdev->dev, &dev_attr_mode);
+	device_remove_file(&pdev->dev, &dev_attr_auto_vbus_src_sel_threshold);
 
 	if (mdwc->dpdm_nb.notifier_call) {
 		regulator_unregister_notifier(mdwc->dpdm_reg, &mdwc->dpdm_nb);
@@ -3942,7 +3972,7 @@ static bool dwc3_use_external_vbus_booster(struct usb_device *udev,
 
 	if (dwc3_is_root_hub_direct_child(udev, dwc) &&
 	    udev->descriptor.bDeviceClass != USB_CLASS_HUB &&
-	    max_power < mdwc->auto_vbus_src_sel)
+	    max_power < mdwc->auto_vbus_src_sel_threshold)
 		return true;
 
 	return false;
@@ -4015,7 +4045,7 @@ static int dwc3_msm_host_notifier(struct notifier_block *nb,
 		}
 	}
 
-	if (mdwc->auto_vbus_src_sel) {
+	if (mdwc->auto_vbus_src_sel_threshold) {
 		if (event == USB_DEVICE_ADD &&
 		    dwc3_use_external_vbus_booster(udev, dwc, mdwc))
 			blocking_notifier_call_chain(&ext_vbus_notifier_list,
