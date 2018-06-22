da300d26f9caabff6387ba5764de5a2117e5b299
Badhri Jagan Sridharan <badhri@google.com>
2018-06-22 14:46:36 -0700

    qpnp-pdphy: signal timed wakeup event
    tReceiverResponse 15 ms
    tSenderResponse 24-30 ms
    
    The partner will hard reset if the SenderResponseTimer
    timesout. This target is hard to meet if the system
    goes into suspend. Hence signal a wakeup event with
    timeout.
    
    Bug: 73295151
    Change-Id: If6cd6110f4e6e18839896549f4e0941b5ed09551
    Signed-off-by: Badhri Jagan Sridharan <badhri@google.com>

--- drivers/usb/pd/qpnp-pdphy.c
+++ drivers/usb/pd/qpnp-pdphy.c
@@ -626,6 +626,7 @@ static irqreturn_t pdphy_msg_tx_irq(int irq, void *data)
 {
 	struct usb_pdphy *pdphy = data;
 
+	pm_wakeup_event(pdphy->dev, PD_ACTIVITY_TIMEOUT_MS);
 	/* TX already aborted by received signal */
 	if (pdphy->tx_status != -EINPROGRESS)
 		return IRQ_HANDLED;
@@ -653,6 +654,7 @@ static irqreturn_t pdphy_msg_rx_discarded_irq(int irq, void *data)
 {
 	struct usb_pdphy *pdphy = data;
 
+	pm_wakeup_event(pdphy->dev, PD_ACTIVITY_TIMEOUT_MS);
 	pdphy->msg_rx_discarded_cnt++;
 
 	return IRQ_HANDLED;
@@ -664,6 +666,7 @@ static irqreturn_t pdphy_sig_rx_irq_thread(int irq, void *data)
 	int ret;
 	struct usb_pdphy *pdphy = data;
 
+	pm_wakeup_event(pdphy->dev, PD_ACTIVITY_TIMEOUT_MS);
 	pdphy->sig_rx_cnt++;
 
 	ret = pdphy_reg_read(pdphy, &rx_status, USB_PDPHY_RX_STATUS, 1);
@@ -695,6 +698,7 @@ static irqreturn_t pdphy_sig_tx_irq_thread(int irq, void *data)
 {
 	struct usb_pdphy *pdphy = data;
 
+	pm_wakeup_event(pdphy->dev, PD_ACTIVITY_TIMEOUT_MS);
 	/* in case of exit from BIST Carrier Mode 2, clear BIST_MODE */
 	pdphy_reg_write(pdphy, USB_PDPHY_BIST_MODE, 0);
 
@@ -726,6 +730,7 @@ static irqreturn_t pdphy_msg_rx_irq(int irq, void *data)
 	int ret;
 	struct usb_pdphy *pdphy = data;
 
+	pm_wakeup_event(pdphy->dev, PD_ACTIVITY_TIMEOUT_MS);
 	pdphy->msg_rx_cnt++;
 
 	ret = pdphy_reg_read(pdphy, &size, USB_PDPHY_RX_SIZE, 1);
